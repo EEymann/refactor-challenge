9c4b5644544b4b5b4fd597386100b8fe267350bc
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-08-31 11:22:41 -0700

    refactor the for loop by creating a function that holds a random number generator and calling it three times inside the for loop to output three RGB color values.

--- js/script.js
+++ js/script.js
@@ -3,11 +3,14 @@ let red;
 let green;
 let blue;
 let randomRGB;
+//Create a function that generates a number from 0-255. In RGB, that is the allowable values for red, green, or blue value.
+const randomValue = () => Math.floor(Math.random() * 256);
 
+//Call the function randomValue three times within the for loop to generate three random numbers that will later be used to turn out three random RGB colors.
 for (let i = 1; i <= 10; i++ ) {
-  red = Math.floor(Math.random() * 256);
-  green = Math.floor(Math.random() * 256);
-  blue = Math.floor(Math.random() * 256);
+  red = randomValue();
+  green = randomValue();
+  blue = randomValue();
   randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
   html += `<div style="background-color: ${randomRGB}">${i}</div>`;
 }
