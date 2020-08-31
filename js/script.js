ec85691325de5a005ed88c7bd89e29075b981922
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-08-31 11:55:09 -0700

    add a reusable function that creates the RGB value and refactor the for loop by calling this function.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 let html = '';
-let red;
-let green;
-let blue;
-let randomRGB;
-//Create a function that generates a number from 0-255. In RGB, that is the allowable values for red, green, or blue value.
+
+//A function that generates a number from 0-255. In RGB, that is the allowable values for red, green, or blue value.
 const randomValue = () => Math.floor(Math.random() * 256);
 
-//Call the function randomValue three times within the for loop to generate three random numbers that will later be used to turn out three random RGB colors.
+//A reusable function that creates the RGB value. Inside the variable color, insert a random red, green or blue value by calling the randomValue function.
+function randomRGB() {
+  const color = `rgb( ${randomValue()}, ${randomValue()}, ${randomValue()} )`;
+  return color;
+
+}
+
+
+//Inside the template literal assigned to the html variable, call the randomRGB function.
 for (let i = 1; i <= 10; i++ ) {
-  red = randomValue();
-  green = randomValue();
-  blue = randomValue();
-  randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
-  html += `<div style="background-color: ${randomRGB}">${i}</div>`;
+  html += `<div style="background-color: ${randomRGB()}">${i}</div>`;
 }
 document.querySelector('main').innerHTML = html;
